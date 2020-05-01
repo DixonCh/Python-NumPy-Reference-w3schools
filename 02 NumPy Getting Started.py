1431c80eafa32ab5c5f9ee5b0db81785b237393d
DixonCh <dixonchaudhary7@gmail.com>
2020-05-01 16:15:29 +0545

    Update 02 NumPy Getting Started.py

--- 02 NumPy Getting Started.py
+++ 02 NumPy Getting Started.py
@@ -1,12 +1,15 @@
+'''
 NumPy Getting Started
 Installation of NumPy
 If you have Python and PIP already installed on a system, then installation of NumPy is very easy.
 
 Install it using this command:
+'''
 
 C:\Users\Your Name>pip install numpy
 If this command fails, then use a python distribution that already has NumPy installed like, Anaconda, Spyder etc.
 
+'''
 Import NumPy
 Once NumPy is installed, import it in your applications by adding the import keyword:
 
@@ -14,11 +17,13 @@ import numpy
 Now Numpy is imported and ready to use.
 
 Example
+'''
 import numpy
 
 arr = numpy.array([1, 2, 3, 4, 5])
 
 print(arr)
+'''
 NumPy as np
 NumPy is usually imported under the np alias.
 
@@ -30,15 +35,18 @@ import numpy as np
 Now the NumPy package can be referred to as np instead of numpy.
 
 Example
+'''
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
 
 print(arr)
+'''
 Checking NumPy Version
 The version string is stored under __version__ attribute.
 
 Example
+'''
 import numpy as np
 
 print(np.__version__)
