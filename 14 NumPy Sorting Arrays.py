a952dd93fea12bfc37b2ea22be94abd2560eeeff
DixonCh <dixonchaudhary7@gmail.com>
2020-05-02 15:55:05 +0545

    Update 14 NumPy Sorting Arrays.py

--- 14 NumPy Sorting Arrays.py
+++ 14 NumPy Sorting Arrays.py
@@ -1,4 +1,4 @@
-NumPy Sorting Arrays
+'''NumPy Sorting Arrays
 Sorting Arrays
 Sorting means putting elements in a ordered sequence.
 
@@ -8,38 +8,42 @@ The NumPy ndarray object has a function called sort(), that will sort a specifie
 
 Example
 Sort the array:
-
+'''
 import numpy as np
 
 arr = np.array([3, 2, 0, 1])
 
 print(np.sort(arr))
+'''
 Note: This method returns a copy of the array, leaving the original array unchanged.
 
 You can also sort arrays of strings, or any other data type:
 
 Example
 Sort the array alphabetically:
+'''
 
 import numpy as np
 
 arr = np.array(['banana', 'cherry', 'apple'])
 
 print(np.sort(arr))
+'''
 Example
 Sort a boolean array:
-
+'''
 import numpy as np
 
 arr = np.array([True, False, True])
 
 print(np.sort(arr))
+'''
 Sorting a 2-D Array
 If you use the sort() method on a 2-D array, both arrays will be sorted:
 
 Example
 Sort a 2-D array:
-
+'''
 import numpy as np
 
 arr = np.array([[3, 2, 4], [5, 0, 1]])
