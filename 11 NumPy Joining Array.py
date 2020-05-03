b439a5267f9db2fb2d9115f0cac3f0fb1bbf0dc4
DixonCh <dixonchaudhary7@gmail.com>
2020-05-03 13:09:50 +0545

    Update 11 NumPy Joining Array.py

--- 11 NumPy Joining Array.py
+++ 11 NumPy Joining Array.py
@@ -25,7 +25,8 @@ arr = np.concatenate((arr1, arr2))
 
 print(arr)
 '''
-Example
+Example:
+
 Join two 2-D arrays along rows (axis=1):
 '''
 import numpy as np
@@ -38,12 +39,14 @@ arr = np.concatenate((arr1, arr2), axis=1)
 
 print(arr)
 '''
-Joining Arrays Using Stack Functions
+Joining Arrays Using Stack Functions:
+
 Stacking is same as concatenation, the only difference is that stacking is done along a new axis.
 
 We can concatenate two 1-D arrays along the second axis which would result in putting them one over the other, ie. stacking.
 
-We pass a sequence of arrays that we want to join to the concatenate() method along with the axis. If axis is not explicitly passed it is taken as 0.
+We pass a sequence of arrays that we want to join to the concatenate() method along with the axis. If axis is not explicitly passed 
+it is taken as 0.
 
 Example
 '''
