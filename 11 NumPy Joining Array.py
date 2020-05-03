3aeb0fdee2e6e3ecf9ae8bd5e24f030f8fd188bd
DixonCh <dixonchaudhary7@gmail.com>
2020-05-03 13:07:45 +0545

    Update 11 NumPy Joining Array.py

--- 11 NumPy Joining Array.py
+++ 11 NumPy Joining Array.py
@@ -7,7 +7,8 @@ Joining means putting contents of two or more arrays in a single array.
 
 In SQL we join tables based on a key, whereas in NumPy we join arrays by axes.
 
-We pass a sequence of arrays that we want to join to the concatenate() function, along with the axis. If axis is not explicitly passed, it is taken as 0.
+We pass a sequence of arrays that we want to join to the concatenate() function, along with the axis. If axis is not explicitly passed, 
+it is taken as 0.
 
 Example:
 
