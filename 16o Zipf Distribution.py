d20c08cd113fee19175aea22a8d91d11768e98e4
DixonCh <dixonchaudhary7@gmail.com>
2020-05-05 13:52:42 +0545

    Update 16o Zipf Distribution.py

--- 16o Zipf Distribution.py
+++ 16o Zipf Distribution.py
@@ -14,16 +14,19 @@ size - The shape of the returned array.
 Example:
 
 Draw out a sample for zipf distribution with distribution parameter 2 with size 2x3:
-
+'''
 from numpy import random
 
 x = random.zipf(a=2, size=(2, 3))
 
 print(x)
-Visualization of Zipf Distribution
+'''
+Visualization of Zipf Distribution:
+
 Sample 1000 points but plotting only ones with value < 10 for more meaningful chart.
 
 Example
+'''
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -32,4 +35,5 @@ x = random.zipf(a=2, size=1000)
 sns.distplot(x[x<10], kde=False)
 
 plt.show()
+'''
 Result
