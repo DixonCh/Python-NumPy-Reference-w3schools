6253af20227cd614e499d2dc3e08e959fce89fd9
DixonCh <dixonchaudhary7@gmail.com>
2020-05-05 13:51:34 +0545

    Update 16o Zipf Distribution.py

--- 16o Zipf Distribution.py
+++ 16o Zipf Distribution.py
@@ -1,7 +1,9 @@
+'''
 Zipf Distribution
 Zipf distritutions are used to sample data based on zipf's law.
 
-Zipf's Law: In a collection the nth common term is 1/n times of the most common term. E.g. 5th common word in english has occurs nearly 1/5th times as of the most used word.
+Zipf's Law: In a collection the nth common term is 1/n times of the most common term. E.g. 5th common word in english has occurs 
+nearly 1/5th times as of the most used word.
 
 It has two parameters:
 
@@ -9,7 +11,8 @@ a - distribution parameter.
 
 size - The shape of the returned array.
 
-Example
+Example:
+
 Draw out a sample for zipf distribution with distribution parameter 2 with size 2x3:
 
 from numpy import random
