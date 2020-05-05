ce10e1ee798e1a8755935742b07468ff59016ddd
DixonCh <dixonchaudhary7@gmail.com>
2020-05-05 13:53:42 +0545

    Update 16n Pareto Distribution.py

--- 16n Pareto Distribution.py
+++ 16n Pareto Distribution.py
@@ -1,3 +1,4 @@
+'''
 Pareto Distribution
 Pareto Distribution
 A distribution following Pareto's law i.e. 80-20 distribution (20% factors cause 80% outcome).
@@ -10,14 +11,16 @@ size - The shape of the returned array.
 
 Example
 Draw out a sample for pareto distribution with shape of 2 with size 2x3:
-
+'''
 from numpy import random
 
 x = random.pareto(a=2, size=(2, 3))
 
 print(x)
+'''
 Visualization of Pareto Distribution
-Example
+Example:
+'''
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
