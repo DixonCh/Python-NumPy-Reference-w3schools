7c6758bda18b167c9fd8e800c829b070c2b6420f
DixonCh <dixonchaudhary7@gmail.com>
2020-05-05 13:55:35 +0545

    Update 16m Rayleigh Distribution.py

--- 16m Rayleigh Distribution.py
+++ 16m Rayleigh Distribution.py
@@ -1,3 +1,4 @@
+'''
 Rayleigh Distribution
 Rayleigh Distribution
 Rayleigh distribution is used in signal processing.
@@ -10,14 +11,16 @@ size - The shape of the returned array.
 
 Example
 Draw out a sample for rayleigh distribution with scale of 2 with size 2x3:
-
+'''
 from numpy import random
 
 x = random.rayleigh(scale=2, size=(2, 3))
 
 print(x)
+'''
 Visualization of Rayleigh Distribution
 Example
+'''
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -25,8 +28,9 @@ import seaborn as sns
 sns.distplot(random.rayleigh(size=1000), hist=False)
 
 plt.show()
+'''
 Result
-
 
 Similarity Between Rayleigh and Chi Square Distribution
 At unit stddev the and 2 degrees of freedom rayleigh and chi square represent the same distributions.
+'''
