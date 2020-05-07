9916b281fbfd58fc93cddf948ea8787cadc36066
DixonCh <dixonchaudhary7@gmail.com>
2020-05-07 16:00:16 +0545

    Update 17 NumPy ufuncs.py

--- 17 NumPy ufuncs.py
+++ 17 NumPy ufuncs.py
@@ -1,3 +1,4 @@
+'''
 NumPy ufuncs
 What are ufuncs?
 ufuncs stands for "Universal Functions" and they are NumPy functions that operates on the ndarray object.
@@ -21,15 +22,17 @@ Converting iterative statements into a vector based operation is called vectoriz
 It is faster as modern CPUs are optimized for such operations.
 
 Add the Elements of Two Lists
+'''
+
 list 1: [1, 2, 3, 4]
 
 list 2: [4, 5, 6, 7]
-
+'''
 One way of doing it is to iterate over both of the lists and then sum each elements.
 
 Example
 Without ufunc, we can use Python's built-in zip() method:
-
+'''
 x = [1, 2, 3, 4]
 y = [4, 5, 6, 7]
 z = []
@@ -37,10 +40,13 @@ z = []
 for i, j in zip(x, y):
   z.append(i + j)
 print(z)
+
+'''
 NumPy has a ufunc for this, called add(x, y) that will produce the same result.
 
 Example
 With ufunc, we can use the add() function:
+'''
 
 import numpy as np
 
