78245c3ce6d0605b6eb26bb653fb19a17af8bd96
DixonCh <dixonchaudhary7@gmail.com>
2020-05-07 16:20:19 +0545

    Update 16d Seaborn.py

--- 16d Seaborn.py
+++ 16d Seaborn.py
@@ -1,7 +1,6 @@
-Seaborn
+'''Seaborn
 Visualize Distributions With Seaborn
 Seaborn is a library that uses Matplotlib underneath to plot graphs. It will be used to visualize random distributions.
-
 
 Install Seaborn.
 If you have Python and PIP already installed on a system, install it using this command:
@@ -11,8 +10,9 @@ If you use Jupyter, install Seaborn using this command:
 
 C:\Users\Your Name>!pip install seaborn
 Distplots
-Distplot stands for distribution plot, it takes as input an array and plots a curve corresponding to the distribution of points in the array.
-
+Distplot stands for distribution plot, it takes as input an array and plots a curve corresponding to the distribution of points in the 
+array.
+'''
 Import Matplotlib
 import the pyplot object of the Matplotlib module in your code using following code
 import matplotlib.pyplot as plt
