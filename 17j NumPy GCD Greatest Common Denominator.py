c4196d529aa516f3ae998ee6806b5a5dc8ba26fe
DixonCh <dixonchaudhary7@gmail.com>
2020-05-07 16:18:48 +0545

    Update 17j NumPy GCD Greatest Common Denominator.py

--- 17j NumPy GCD Greatest Common Denominator.py
+++ 17j NumPy GCD Greatest Common Denominator.py
@@ -21,12 +21,14 @@ print(x)
 '''
 Returns: 3 because that is the highest number both numbers can be divided by (6/3=2 and 9*3=3).
 
-Finding GCD in Arrays
+Finding GCD in Arrays:
+
 To find the Highest Common Factor of all values in an array, you can use the reduce() method.
 
 The reduce() method will use the ufunc, in this case the gcd() function, on each element, and reduce the array by one dimension.
 
-Example
+Example:
+
 Find the GCD for all of the numbers in following array:
 '''
 import numpy as np
