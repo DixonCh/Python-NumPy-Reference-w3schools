7eea2c0dc277d723f46fec6dafb9ea45bcaaec10
DixonCh <dixonchaudhary7@gmail.com>
2020-05-08 13:53:38 +0545

    Update 17l NumPy Hyperbolic Functions.py

--- 17l NumPy Hyperbolic Functions.py
+++ 17l NumPy Hyperbolic Functions.py
@@ -1,6 +1,8 @@
 '''
-NumPy Hyperbolic Functions
-Hyperbolic Functions
+NumPy Hyperbolic Functions:
+
+Hyperbolic Functions:
+
 NumPy provides the ufuncs sinh(), cosh() and tanh() that take values in radians and produce the corresponding sinh, cosh and tanh values..
 
 Example
