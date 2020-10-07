134a7f4c576ca62ce7b0a369eb8fd407c22d0d46
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-10-07 08:25:52 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -8,7 +8,8 @@ You can access an array element by referring to its index number.
 
 The indexes in NumPy arrays start with 0, meaning that the first element has index 0, and the second has index 1 etc.
 
-Example
+Example:
+  
 Get the first element from the following array:
 
 import numpy as np
@@ -37,7 +38,8 @@ print(arr[2] + arr[3])
 Access 2-D Arrays
 To access elements from 2-D arrays we can use comma separated integers representing the dimension and the index of the element.
 
-Example
+Example:
+  
 Access the 2nd element on 1st dim:
 
 import numpy as np
