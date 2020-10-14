26c334161fa6a833e50dc3dfca166d9bcccc4d0e
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-10-14 19:27:03 +0545

    Update 15 NumPy Filter Array.py

--- 15 NumPy Filter Array.py
+++ 15 NumPy Filter Array.py
@@ -10,7 +10,8 @@ A boolean index list is a list of booleans corresponding to indexes in the array
 
 If the value at an index is True that element is contained in the filtered array, if the value at that index is False that element is excluded from the filtered array.
 
-Example
+Example:
+  
 Create an array from the elements on index 0 and 2:
 
 import numpy as np
@@ -26,10 +27,12 @@ The example above will return [41, 43], why?
 
 Because the new filter contains only the values where the filter array had the value True, in this case, index 0 and 2.
 
-Creating the Filter Array
+Creating the Filter Array:
+  
 In the example above we hard-coded the True and False values, but the common use is to create a filter array based on conditions.
 
-Example
+Example:
+  
 Create a filter array that will return only values higher than 42:
 
 import numpy as np
@@ -51,6 +54,7 @@ newarr = arr[filter_arr]
 
 print(filter_arr)
 print(newarr)
+
 Example
 Create a filter array that will return only even elements from the original array:
 
