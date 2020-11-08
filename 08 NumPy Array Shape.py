b766359ffee8b492ddc27c7a7e50c810ee8f8fa4
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-11-08 10:07:35 +0545

    Update 08 NumPy Array Shape.py

--- 08 NumPy Array Shape.py
+++ 08 NumPy Array Shape.py
@@ -34,6 +34,7 @@ print(arr)
 print('shape of array :', arr.shape)
 '''
 What does the shape tuple represent?
+
 Integers at every index tells about the number of elements the corresponding dimension has.
 
 In the example above at index-4 we have value 4, so we can say that 5th ( 4 + 1 th) dimension has 4 elements.
