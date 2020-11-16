ca4a01d215e6afae572ef3173a722755fb131cc1
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2020-11-16 15:55:55 +0545

    Update 14 NumPy Sorting Arrays.py

--- 14 NumPy Sorting Arrays.py
+++ 14 NumPy Sorting Arrays.py
@@ -44,7 +44,8 @@ arr = np.array([True, False, True])
 
 print(np.sort(arr))
 '''
-Sorting a 2-D Array
+Sorting a 2-D Array:
+
 If you use the sort() method on a 2-D array, both arrays will be sorted:
 
 Example:
