eb6be7c69fbcca0ea37ebfde4f7348d0f5e6cb8a
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-01-14 09:55:52 +0545

    Update 16o Zipf Distribution.py

--- 16o Zipf Distribution.py
+++ 16o Zipf Distribution.py
@@ -1,8 +1,10 @@
 '''
-Zipf Distribution
+Zipf Distribution:
+
 Zipf distritutions are used to sample data based on zipf's law.
 
-Zipf's Law: In a collection the nth common term is 1/n times of the most common term. E.g. 5th common word in english has occurs 
+Zipf's Law: In a collection the nth common term is 1/n times of the most common term. E.g. 5th common word in english has occurs
+
 nearly 1/5th times as of the most used word.
 
 It has two parameters:
