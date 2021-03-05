8a72a4a91eae8cf23606d0f0cb1f2b7b8ef09c3d
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-03-05 07:38:23 +0545

    Update 16m Rayleigh Distribution.py

--- 16m Rayleigh Distribution.py
+++ 16m Rayleigh Distribution.py
@@ -9,7 +9,8 @@ scale - (standard deviation) decides how flat the distribution will be default 1
 
 size - The shape of the returned array.
 
-Example
+Example:
+
 Draw out a sample for rayleigh distribution with scale of 2 with size 2x3:
 '''
 from numpy import random
@@ -18,8 +19,9 @@ x = random.rayleigh(scale=2, size=(2, 3))
 
 print(x)
 '''
-Visualization of Rayleigh Distribution
-Example
+Visualization of Rayleigh Distribution:
+
+Example:
 '''
 from numpy import random
 import matplotlib.pyplot as plt
