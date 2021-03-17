8d7bb1e42c912a8edd7706925c292af6d34ec246
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-03-17 08:13:59 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -41,7 +41,8 @@ nested array: are arrays that have arrays as their elements.
 0-D Arrays
 0-D arrays, or Scalars, are the elements in an array. Each value in an array is a 0-D array.
 
-Example
+Example:
+
 Create a 0-D array with value 42
 '''
 
@@ -66,14 +67,16 @@ arr = np.array([1, 2, 3, 4, 5])
 
 print(arr)
 '''
-2-D Arrays
+2-D Arrays:
+
 An array that has 1-D arrays as its elements is called a 2-D array.
 
 These are often used to represent matrix or 2nd order tensors.
 
 NumPy has a whole sub module dedicated towards matrix operations called numpy.mat
 
-Example
+Example:
+
 Create a 2-D array containing two arrays with the values 1,2,3 and 4,5,6:
 '''
 import numpy as np
