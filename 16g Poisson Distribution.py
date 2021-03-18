cc3c00c3b663e89aba583e40fc8621aef6447fcf
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-03-18 07:43:38 +0545

    Update 16g Poisson Distribution.py

--- 16g Poisson Distribution.py
+++ 16g Poisson Distribution.py
@@ -59,7 +59,8 @@ The difference is very subtle it is that, binomial distribution is for discrete 
 
 But for very large n and near-zero p binomial distribution is near identical to poisson distribution such that n * p is nearly equal to lam.
 
-Example
+Example:
+  
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -68,4 +69,3 @@ sns.distplot(random.binomial(n=1000, p=0.01, size=1000), hist=False, label='bino
 sns.distplot(random.poisson(lam=10, size=1000), hist=False, label='poisson')
 
 plt.show()
-Result
