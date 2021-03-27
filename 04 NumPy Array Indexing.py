ef7345223703eec1d3119120d391af248042d842
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-03-27 13:06:57 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -49,7 +49,9 @@ import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
 print('2nd element on 1st dim: ', arr[0, 1])
-Example
+
+Example:
+  
 Access the 5th element on 2nd dim:
 
 import numpy as np
