68b214c7472abccf76b5be0933a646c2312002a1
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-04-27 07:01:27 +0545

    Update 08 NumPy Array Shape.py

--- 08 NumPy Array Shape.py
+++ 08 NumPy Array Shape.py
@@ -13,7 +13,6 @@ Example:
 
 Print the shape of a 2-D array:
 '''
-
 import numpy as np
 
 arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
@@ -25,6 +24,7 @@ The example above returns (2, 4), which means that the array has 2 dimensions, a
 Example:
 
 Create an array with 5 dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4:
+
 '''
 import numpy as np
 
