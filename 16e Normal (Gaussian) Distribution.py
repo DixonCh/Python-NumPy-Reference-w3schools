29f18f3fe8c3e10dad3eaf61babed844472bcd24
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-05-11 07:51:38 +0545

    Update 16e Normal (Gaussian) Distribution.py

--- 16e Normal (Gaussian) Distribution.py
+++ 16e Normal (Gaussian) Distribution.py
@@ -12,11 +12,11 @@ Use the random.normal() method to get a Normal Data Distribution.
 
 It has three parameters:
 
-loc - (Mean) where the peak of the bell exists.
+loc: (Mean) where the peak of the bell exists.
 
-scale - (Standard Deviation) how flat the graph distribution should be.
+scale: (Standard Deviation) how flat the graph distribution should be.
 
-size - The shape of the returned array.
+size: The shape of the returned array.
 
 Example:
 
