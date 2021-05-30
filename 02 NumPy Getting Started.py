66facac109c2394adf4f0ddeee405f4e19c666a4
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-05-30 12:51:48 +0545

    Update 02 NumPy Getting Started.py

--- 02 NumPy Getting Started.py
+++ 02 NumPy Getting Started.py
@@ -28,6 +28,7 @@ arr = numpy.array([1, 2, 3, 4, 5])
 print(arr)
 '''
 NumPy as np
+
 NumPy is usually imported under the np alias.
 
 alias: In Python alias are an alternate name for referring to the same thing.
@@ -45,10 +46,11 @@ arr = np.array([1, 2, 3, 4, 5])
 
 print(arr)
 '''
-Checking NumPy Version
+Checking NumPy Version:
+
 The version string is stored under __version__ attribute.
 
-Example
+Example:
 '''
 import numpy as np
 
