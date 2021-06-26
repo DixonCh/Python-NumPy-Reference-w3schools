8b50d6dffcca735bd23e0f91bc2b5891d5bc6cc9
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-06-26 16:58:46 +0545

    Update 16 NumPy Random.py

--- 16 NumPy Random.py
+++ 16 NumPy Random.py
@@ -16,6 +16,7 @@
 '''Yes. In order to generate a truly random number on our computers we need to get the random data from some outside source. This outside 
 source is generally our keystrokes, mouse movements, data on network etc.
 
+
 We do not need truly random numbers, unless its related to security (e.g. encryption keys) or the basis of application is the randomness (e.g. Digital roulette wheels).
 
 In this tutorial we will be using pseudo random numbers.
@@ -50,6 +51,7 @@ x = random.rand()
 
 print(x)
 '''
+
 Generate Random Array:
 
 In NumPy we work with arrays, and you can use the two methods from the above examples to make random arrays.
