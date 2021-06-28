78a9a4281e7450f66c7b6618d9d3e332c46ee645
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-06-28 08:15:05 +0545

    Update 16 NumPy Random.py

--- 16 NumPy Random.py
+++ 16 NumPy Random.py
@@ -76,12 +76,14 @@ Example:
 
 Generate a 2-D array with 3 rows, each row containing 5 random integers from 0 to 100:
 '''
+
 from numpy import random
 
 x = random.randint(100, size=(3, 5))
 
 print(x)
 '''
+
 Floats:
 
 The rand() method also allows you to specify the shape of the array.
