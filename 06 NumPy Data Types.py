8c229728e7032b3283c89c4aba41dbfc2acad270
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-10-03 10:06:28 +0545

    Update 06 NumPy Data Types.py

--- 06 NumPy Data Types.py
+++ 06 NumPy Data Types.py
@@ -53,6 +53,7 @@ arr = np.array(['apple', 'banana', 'cherry'])
 
 print(arr.dtype)
 '''
+
 Creating Arrays With a Defined Data Type:
 
 We use the array() function to create arrays, this function can take an optional argument: dtype that allows us to define the expected data type of the array elements:
@@ -61,6 +62,7 @@ Example:
 
 Create an array with data type string:
 '''
+
 import numpy as np
 
 arr = np.array([1, 2, 3, 4], dtype='S')
