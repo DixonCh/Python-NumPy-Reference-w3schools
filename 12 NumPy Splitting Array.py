7c41bf2a4e230f4ebaaf17015c5917edab16e3b3
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-10-15 17:13:10 +0545

    Update 12 NumPy Splitting Array.py

--- 12 NumPy Splitting Array.py
+++ 12 NumPy Splitting Array.py
@@ -104,6 +104,7 @@ Example:
 
 Split the 2-D array into three 2-D arrays along rows.
 '''
+
 import numpy as np
 
 arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
