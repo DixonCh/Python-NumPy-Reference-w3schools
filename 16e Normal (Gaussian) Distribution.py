1a99e4d22f0451077fe99cb56f19079c382c4648
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-10-17 18:51:00 +0545

    Update 16e Normal (Gaussian) Distribution.py

--- 16e Normal (Gaussian) Distribution.py
+++ 16e Normal (Gaussian) Distribution.py
@@ -43,7 +43,9 @@ Visualization of Normal Distribution:
 Example:
   
 from numpy import random
+
 import matplotlib.pyplot as plt
+
 import seaborn as sns
 
 sns.distplot(random.normal(size=1000), hist=False)
