239e74fe989e1549863c6dd7ba007f0f34b786b5
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-11-11 07:41:42 +0545

    Update 13 NumPy Searching Arrays.py

--- 13 NumPy Searching Arrays.py
+++ 13 NumPy Searching Arrays.py
@@ -50,7 +50,8 @@ print(x)
 '''
 Search Sorted:
 
-There is a method called searchsorted() which performs a binary search in the array, and returns the index where the specified value would be inserted to maintain the 
+There is a method called searchsorted() which performs a binary search in the array, and returns the index where the specified value would be inserted to maintain the
+
 search order.
 
 The searchsorted() method is assumed to be used on sorted arrays.
