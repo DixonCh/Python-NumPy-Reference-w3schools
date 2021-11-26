612d0e701b1205bbc28f6d96f0735c8e8022eeba
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-11-26 16:20:29 +0545

    Update 08 NumPy Array Shape.py

--- 08 NumPy Array Shape.py
+++ 08 NumPy Array Shape.py
@@ -20,13 +20,11 @@ arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
 print(arr.shape)
 '''
-
 The example above returns (2, 4), which means that the array has 2 dimensions, and each dimension has 4 elements.
 
 Example:
 
 Create an array with 5 dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4:
-
 '''
 import numpy as np
 
