627ad36825062137ebfa5816e9fcbcdacb5e2b15
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-01-08 09:26:37 +0545

    Update 02 NumPy Getting Started.py

--- 02 NumPy Getting Started.py
+++ 02 NumPy Getting Started.py
@@ -9,6 +9,7 @@ Install it using this command:
 '''
 
 C:\Users\Your Name>pip install numpy
+  
 If this command fails, then use a python distribution that already has NumPy installed like, Anaconda, Spyder etc.
 
 '''
