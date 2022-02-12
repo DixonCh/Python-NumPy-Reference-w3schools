2617a9d0cf2d5243de7062857875cbb0229dae36
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-02-12 07:35:18 +0545

    Update 16 NumPy Random.py

--- 16 NumPy Random.py
+++ 16 NumPy Random.py
@@ -15,6 +15,7 @@
 #Can we make truly random numbers?
 
 '''Yes. In order to generate a truly random number on our computers we need to get the random data from some outside source. This outside 
+
 source is generally our keystrokes, mouse movements, data on network etc.
 
 
