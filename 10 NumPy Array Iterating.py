81c5caf457d64585829f18e0989c6b8893d26b8a
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-02-23 10:46:26 +0545

    Update 10 NumPy Array Iterating.py

--- 10 NumPy Array Iterating.py
+++ 10 NumPy Array Iterating.py
@@ -17,7 +17,6 @@ import numpy as np
 arr = np.array([1, 2, 3])
 
 for x in arr:
-  
   print(x)
   '''
 Iterating 2-D Arrays:
@@ -86,7 +85,7 @@ for x in arr:
     for z in y:
       print(z)
       '''
-Iterating Arrays Using nditer()
+Iterating Arrays Using nditer():
 
 The function nditer() is a helping function that can be used from very basic to very advanced iterations. It solves some basic issues which we face in iteration, 
 lets go through it with examples.
