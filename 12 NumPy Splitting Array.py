e0901daeff8baa63c64b72be86289959e45ca030
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-03-13 11:05:34 +0545

    Update 12 NumPy Splitting Array.py

--- 12 NumPy Splitting Array.py
+++ 12 NumPy Splitting Array.py
@@ -19,8 +19,8 @@ arr = np.array([1, 2, 3, 4, 5, 6])
 newarr = np.array_split(arr, 3)
 
 print(newarr)
-
 '''
+
 Note: The return value is an array containing three arrays.
 
 If the array has less elements than required, it will adjust from the end accordingly.
@@ -38,6 +38,7 @@ newarr = np.array_split(arr, 4)
 
 print(newarr)
 '''
+
 Note: We also have the method split() available but it will not adjust the elements when elements are less in source array for splitting like in example above, array_split()
 
 worked properly but split() would fail.
