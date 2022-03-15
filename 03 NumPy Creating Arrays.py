eb8e9c7cc3d6b02dc96eb3a6c701f3d0eeb53af4
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-03-15 10:35:55 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -8,7 +8,6 @@ We can create a NumPy ndarray object by using the array() function.
 
 Example:
 '''
-
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
@@ -54,6 +53,7 @@ arr = np.array(42)
 
 print(arr)
 '''
+
 1-D Arrays
 An array that has 0-D arrays as its elements is called uni-dimensional or 1-D array.
 
@@ -94,7 +94,8 @@ An array that has 2-D arrays (matrices) as its elements is called 3-D array.
 
 These are often used to represent a 3rd order tensor.
 
-Example
+Example:
+
 Create a 3-D array with two 2-D arrays, both containing two arrays with the values 1,2,3 and 4,5,6:
 '''
 import numpy as np
