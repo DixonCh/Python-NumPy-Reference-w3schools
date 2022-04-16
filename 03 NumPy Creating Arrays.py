93af6cf5d22886625f188f77f09fa8c2946255c3
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-04-16 20:05:28 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -45,6 +45,7 @@ nested array: are arrays that have arrays as their elements.
 0-D arrays, or Scalars, are the elements in an array. Each value in an array is a 0-D array.
 
 Example:
+
 Create a 0-D array with value 42
 '''
 
