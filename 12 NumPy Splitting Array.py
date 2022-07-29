95f035c49a2a266731e317bfaeb524b871714fce
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-07-29 09:04:18 +0545

    Update 12 NumPy Splitting Array.py

--- 12 NumPy Splitting Array.py
+++ 12 NumPy Splitting Array.py
@@ -1,4 +1,5 @@
 '''NumPy Splitting Array:
+
 Splitting NumPy Arrays:
 
 Splitting is reverse operation of Joining.
@@ -11,6 +12,7 @@ Example:
 
 Split the array in 3 parts:
 '''
+
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5, 6])
