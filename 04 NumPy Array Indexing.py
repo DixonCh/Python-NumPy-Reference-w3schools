467575caf2c358bae02e60626dd664760b7f8b6c
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-08-31 16:30:38 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -62,7 +62,6 @@ arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('5th element on 2nd dim: ', arr[1, 4])
 
 Access 3-D Arrays:
-  
 To access elements from 3-D arrays we can use comma separated integers representing the dimensions and the index of the element.
 
 Example:
@@ -85,7 +84,9 @@ The first number represents the first dimension, which contains two arrays:
 [[1, 2, 3], [4, 5, 6]]
 and:
 [[7, 8, 9], [10, 11, 12]]
+
 Since we selected 0, we are left with the first array:
+  
 [[1, 2, 3], [4, 5, 6]]
 
 The second number represents the second dimension, which also contains two arrays:
