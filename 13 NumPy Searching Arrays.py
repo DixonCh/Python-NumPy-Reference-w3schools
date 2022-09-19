70aef3407aced715e1ce1226ad4d92007744b64d
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-09-19 07:49:00 +0545

    Update 13 NumPy Searching Arrays.py

--- 13 NumPy Searching Arrays.py
+++ 13 NumPy Searching Arrays.py
@@ -100,7 +100,6 @@ Example explained: The number 7 should be inserted on index 2 to remain the sort
 The method starts the search from the right and returns the first index where the number 7 is no longer less than the next value.
 
 Multiple Values:
-
 To search for more than one value, use an array with the specified values.
 
 Example:
@@ -115,6 +114,5 @@ arr = np.array([1, 3, 5, 7])
 x = np.searchsorted(arr, [2, 4, 6])
 
 print(x)
-
 #The return value is an array: [1 2 3] containing the three indexes where 2, 4, 6 would be inserted in the original array to maintain the order.
 
