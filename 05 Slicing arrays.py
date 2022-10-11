44b589fe00b960f0237d452cf9676c441995ef7c
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-10-11 10:30:08 +0545

    Update 05 Slicing arrays.py

--- 05 Slicing arrays.py
+++ 05 Slicing arrays.py
@@ -14,7 +14,6 @@ If we don't pass end its considered length of array in that dimension
 If we don't pass step its considered 1
 
 Example:
-
 Slice elements from index 1 to index 5 from the following array:
 '''
 import numpy as np
