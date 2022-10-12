37df20d541d4916447919a0fb13bc5d1636a4f9c
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-10-12 10:28:06 +0545

    Update 10 NumPy Array Iterating.py

--- 10 NumPy Array Iterating.py
+++ 10 NumPy Array Iterating.py
@@ -12,6 +12,7 @@ Example:
 
 Iterate on the elements of the following 1-D array:
 '''
+
 import numpy as np
 
 arr = np.array([1, 2, 3])
@@ -20,6 +21,7 @@ for x in arr:
   print(x)
   '''
 Iterating 2-D Arrays:
+
 In a 2-D array it will go through all the rows.
 
 Example:
