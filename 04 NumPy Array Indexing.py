1527f978abcdb730f56d56f198434850ede58cc5
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2022-12-15 07:57:38 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -9,7 +9,8 @@ You can access an array element by referring to its index number.
 The indexes in NumPy arrays start with 0, meaning that the first element has index 0, and the second has index 1 etc.
 
 Example: 
-  Get the first element from the following array:
+
+Get the first element from the following array:
   
 import numpy as np
 
