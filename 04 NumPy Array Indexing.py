46bf1af1f38b2db957175b1500550d6a04ccc1ad
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-01-16 18:47:55 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -78,8 +78,7 @@ arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 print(arr[0, 1, 2])
 
 Example Explained:
-  
-arr[0, 1, 2] prints the value 6.
+  arr[0, 1, 2] prints the value 6.
 
 And this is why:
 
