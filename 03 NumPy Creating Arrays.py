0ae4e105077b4766992ae7d839071ecd7f8cde57
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-02-17 21:30:24 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -110,6 +110,7 @@ arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
 print(arr)
 '''
+
 Check Number of Dimensions?
 
 NumPy Arrays provides the ndim attribute that returns an integer that tells us how many dimensions the array have.
