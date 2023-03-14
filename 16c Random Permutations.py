a4863f408ff9849bf5e1f8ebd398fc26913dd627
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-03-14 10:00:38 +0545

    Update 16c Random Permutations.py

--- 16c Random Permutations.py
+++ 16c Random Permutations.py
@@ -37,4 +37,5 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 
 print(random.permutation(arr))
+
 The permutation() method returns a re-arranged array (and leaves the original array un-changed).
