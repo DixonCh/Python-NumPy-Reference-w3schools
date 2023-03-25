a82a8e3854bf903da0a0100d68fc06d47c51aea7
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-03-25 21:01:39 +0545

    Update 08 NumPy Array Shape.py

--- 08 NumPy Array Shape.py
+++ 08 NumPy Array Shape.py
@@ -21,7 +21,6 @@ arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 print(arr.shape)
 
 '''
-
 The example above returns (2, 4), which means that the array has 2 dimensions, and each dimension has 4 elements.
 
 Example:
