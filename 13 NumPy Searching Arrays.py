85065f90a4f35e3464a37ef54aeef2fb6a2dc883
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-06-18 08:46:05 +0545

    Update 13 NumPy Searching Arrays.py

--- 13 NumPy Searching Arrays.py
+++ 13 NumPy Searching Arrays.py
@@ -117,5 +117,6 @@ arr = np.array([1, 3, 5, 7])
 x = np.searchsorted(arr, [2, 4, 6])
 
 print(x)
+
 #The return value is an array: [1 2 3] containing the three indexes where 2, 4, 6 would be inserted in the original array to maintain the order.
 
