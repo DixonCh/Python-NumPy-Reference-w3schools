642efe436cb65d83f6bb03237acf20728ae03c5b
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2023-09-22 17:15:40 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -7,7 +7,6 @@ NumPy is used to work with arrays. The array object in NumPy is called ndarray.
 We can create a NumPy ndarray object by using the array() function.
 
 Example:
-
 '''
 import numpy as np
 
