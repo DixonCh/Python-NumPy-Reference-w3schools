09b5e830d3b7dbc2e46383d1d1e39bfd557d3341
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2024-05-19 08:56:17 +0545

    Update 09 NumPy Array Reshaping.py

--- 09 NumPy Array Reshaping.py
+++ 09 NumPy Array Reshaping.py
@@ -55,7 +55,6 @@ Example:
 
 Try converting 1D array with 8 elements to a 2D array with 3 elements in each dimension (will raise an error):
 '''
-
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
