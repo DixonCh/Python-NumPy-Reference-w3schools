1e06efad2347435a38dbbdd656a8b2d463a628f5
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2024-06-12 08:52:12 +0545

    Update 16i Logistic Distribution.py

--- 16i Logistic Distribution.py
+++ 16i Logistic Distribution.py
@@ -13,7 +13,6 @@ scale - standard deviation, the flatness of distribution. Default 1.
 size - The shape of the returned array.
 
 Example:
-  
 Draw 2x3 samples from a logistic distribution with mean at 1 and stddev 2.0:
 
 from numpy import random
