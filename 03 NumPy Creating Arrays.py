0f098e6303a642f1a690c4bc3aac5c57c6321c67
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2025-09-02 18:20:33 +0545

    Update 03 NumPy Creating Arrays.py

--- 03 NumPy Creating Arrays.py
+++ 03 NumPy Creating Arrays.py
@@ -9,7 +9,6 @@ We can create a NumPy ndarray object by using the array() function.
 Example:
 
 '''
-
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
@@ -147,3 +146,4 @@ print(arr)
 print('number of dimensions :', arr.ndim)
 
 #In this array the innermost dimension (5th dim) has 4 elements, the 4th dim has 1 element that is the vector, the 3rd dim has 1 element that is the matrix with the vector, the 2nd dim has 1 element that is 3D array and 1st dim has 1 element that is a 4D array.
+
