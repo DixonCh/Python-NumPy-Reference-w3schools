fc2a958fe70ea79e4aaaef533024ad63d5154a40
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2025-10-24 08:57:27 +0545

    Update 05 Slicing arrays.py

--- 05 Slicing arrays.py
+++ 05 Slicing arrays.py
@@ -65,6 +65,7 @@ print(arr[-3:-1])
 '''
 
 STEP:
+
 Use the step value to determine the step of the slicing:
 
 Example:
@@ -122,3 +123,4 @@ import numpy as np
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
 print(arr[0:2, 1:4])
+
