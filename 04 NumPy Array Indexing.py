faacbb2654f9945a13e24768af74f205657d885d
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2025-12-29 22:24:23 +0545

    Update 04 NumPy Array Indexing.py

--- 04 NumPy Array Indexing.py
+++ 04 NumPy Array Indexing.py
@@ -16,6 +16,7 @@ import numpy as np
 arr = np.array([1, 2, 3, 4])
 
 print(arr[0])
+
 Example:
   
 Get the second element from the following array.
@@ -117,4 +118,5 @@ import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
 print('Last element from 2nd dim: ', arr[1, -1])
+
 
